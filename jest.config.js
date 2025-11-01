4f41de8c4760abab06142ca6e2026102edea9fa0
Jay Kapoor <jkap86@gmail.com>
2025-10-31 21:35:29 -0400

    test: ignore template test files in jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'TEMPLATE_.*\\.test\\.ts$', // Exclude template files
+  ],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',

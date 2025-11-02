e2d5888941149ed106f60047bc0867a7eb9f8e34
Jay Kapoor <jkap86@gmail.com>
2025-11-01 23:43:59 -0400

    feat: update password requirements to min 6 chars with number or special char

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -25,8 +25,10 @@ export const registerValidator: ValidationChain[] = [
     .withMessage("Email must be less than 255 characters"),
 
   body("password")
-    .isLength({ min: 8, max: 128 })
-    .withMessage("Password must be at least 8 characters"),
+    .isLength({ min: 6, max: 128 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/(?=.*[\d@$!%*?&])/)
+    .withMessage("Password must contain at least one number or special character"),
 
   body("phone_number")
     .optional({ nullable: true })
